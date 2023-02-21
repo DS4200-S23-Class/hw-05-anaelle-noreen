567bfe7b529b2bf1a8ed1bcf7543d5254d93cfd7
noreenh21 <hossain.n@northeastern.edu>
2023-02-21 11:03:37 -0500

    Updated Dropdowns and Cleaner code

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
             
+// Set Frame dimensions for visualizations
 const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500; 
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
-
+// ----------- SCATTERPLOT PROCESSING ------------------
+// Make svg to house visualization
 const FRAME1 = d3.select("#scatterplot") 
 .append("svg") 
     .attr("height", FRAME_HEIGHT)   
@@ -13,29 +15,51 @@ const FRAME1 = d3.select("#scatterplot")
 // Scatterplot function
 d3.csv("data/scatter-data.csv").then((data) => { 
 
-    // let's check our data
-    console.log(data); //Notice this data has 3 columns
-                        // to access data in a column, use .
-
+    // x-Scale
     let x = d3.scaleLinear()
     .domain([0, 10])
     .range([ 0, FRAME_WIDTH - MARGINS.left - MARGINS.right]);
-    
     // Add X-axis
     FRAME1.append("g")
     .attr("transform", "translate(" +  MARGINS.left +","+ (FRAME_HEIGHT - MARGINS.top) + ")")
     .call(d3.axisBottom(x));
 
-    // Add Y axis
+    // y-Scale
     let y = d3.scaleLinear()
     .domain([0, 10])
     .range([FRAME_HEIGHT- MARGINS.top - MARGINS.bottom, 0]);
-
+    // Add Y axis
     FRAME1.append("g")
     .attr("transform", "translate(" + MARGINS.left + ","+ MARGINS.top +")")
     .call(d3.axisLeft(y));
 
-    // Add dots
+    // Function to dd event listeners to all circles in visualization
+    function circleEvents(){
+        FRAME1.selectAll(".circles")
+        // When hovering, change fill of the circle, and when the mouse leaves, change it back
+        .on("mouseover", function(d) {
+            d3.select(this).style("fill", "red");
+            })
+            .on("mouseout", function(d) {
+            d3.select(this).style("fill", "lightsalmon");
+            })
+            // When the circle is clicked, update lastClicked and the stroke
+            .on("click", function(d){
+            // Update last clicked in text when circle is clicked
+            let last_clicked = d3.select(this).attr("id")
+            d3.select("#coordinates").text(last_clicked)
+            // Check if stroke exists, and change accordingly
+            if(d3.select(this).style('stroke') == '#006080'){
+                d3.select(this).style("stroke", "none");
+            }
+            else{
+                d3.select(this).style("stroke", "#006080");
+                d3.select(this).style("stroke-width", "3px");
+            }
+        });
+    };
+
+    // Add initial points from data
     FRAME1.append('g')
     .selectAll("dot")
     .data(data)
@@ -47,140 +71,115 @@ d3.csv("data/scatter-data.csv").then((data) => {
         .attr("cy", (d) =>{ return y(d.y) + MARGINS.top; } )
         .attr("r", 5)
         .style("fill", "lightsalmon");
+    // Add Event listeners to them
+    circleEvents();
 
+    // Data for dropdown menus
+    let dropdown = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+    //   Populate X selection dropdown menu
+    d3.select("#selectX")
+        .selectAll('myOptions')
+        .data(dropdown)
+        .enter()
+        .append('option')
+        .text(function (d) { return d; }) // text showed in the menu
+        .attr("value", function (d) { return d; });
 
-    FRAME1.selectAll(".circles")
-    .on("mouseover", function(d) {
-        d3.select(this).style("fill", "red");
-      })
-      .on("mouseout", function(d) {
-        d3.select(this).style("fill", "lightsalmon");
-      })
-      .on("click", function(d){
-        // Update last clicked in text when circle is clicked
-        let last_clicked = d3.select(this).attr("id")
-        d3.select("#coordinates").text(last_clicked)
-
-        if(d3.select(this).style('stroke') == 'blue'){
-            d3.select(this).style("stroke", "none");
-        }
-        else{
-            d3.select(this).style("stroke", "blue");
-            d3.select(this).style("stroke-width", "3px");
-
-        }
-      }); //add event listeners
-
-
-
-      console.log("lol")
-      d3.selectAll(".button")
-      .on("click", function(d){
-        let xVal = document.getElementById("xRange").value
-        let yVal = document.getElementById("yRange").value
-        console.log("lol")
-        console.log(xVal)
-        console.log(yVal)
-
+    //   Populate X selection dropdown menu
+    d3.select("#selectY")
+        .selectAll('myOptions')
+        .data(dropdown)
+        .enter()
+        .append('option')
+        .text(function (d) { return d; }) 
+        .attr("value", function (d) { return d; });
+      
+    // Select the addpoint button, and when clicked, get the values from dropdowns and add them to the plot
+    d3.selectAll(".button")
+        .on("click", function(d){
+        // Get the values from dropdowns
+        let xVal = document.getElementById("selectX").value
+        let yVal = document.getElementById("selectY").value
+        
+        // Add correspoinding circles in the location specified
         FRAME1.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-        .attr("class", "circles")
-        .attr("id", (d) => {return "(" + xVal + ", " + yVal + ")"} )
-        .attr("cx", (d) => { return x(xVal) + MARGINS.left; } )
-        .attr("cy", (d) =>{ return y(yVal) + MARGINS.top; } )
-        .attr("r", 5)
-        .style("fill", "lightsalmon")
-        .on("mouseover", function(d) {
-            d3.select(this).style("fill", "red");
-          })
-          .on("mouseout", function(d) {
-            d3.select(this).style("fill", "lightsalmon");
-          })
-          .on("click", function(d){
-            // Update last clicked in text when circle is clicked
-            let last_clicked = d3.select(this).attr("id")
-            d3.select("#coordinates").text(last_clicked)
-    
-            if(d3.select(this).style('stroke') == 'blue'){
-                d3.select(this).style("stroke", "none");
-            }
-            else{
-                d3.select(this).style("stroke", "blue");
-                d3.select(this).style("stroke-width", "3px");
-    
-            }
-          });
-      })
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("class", "circles")
+            .attr("id", (d) => {return "(" + xVal + ", " + yVal + ")"} )
+            .attr("cx", (d) => { return x(xVal) + MARGINS.left; } )
+            .attr("cy", (d) =>{ return y(yVal) + MARGINS.top; } )
+            .attr("r", 5)
+            .style("fill", "lightsalmon");
+        // Update event listeners to the new circles
+        circleEvents();
+
+    });
+});  
 
 
-  });  
 
-
-// Bar Chart 
+// -------------------------- BAR CHART PROCESSING --------------------------------------- 
+// Add new svg to house the bar chart
 const FRAME2 = d3.select("#barchart") 
 .append("svg") 
     .attr("height", FRAME_HEIGHT)   
     .attr("width", FRAME_WIDTH)
     .attr("class", "frame"); 
 
-
+// Bar Chart function
 d3.csv("data/bar-data.csv").then((data) => {
-    console.log(data)
 
-    // X Axis
+    // Add X Axis
     let x = d3.scaleBand()
         .range([ 0, FRAME_WIDTH - MARGINS.left - MARGINS.right ])
         .domain(data.map(function(d) { return d.category; }))
         .padding(0.2);
-
     FRAME2.append("g")
         .attr("transform", "translate(" +  MARGINS.left +","+ (FRAME_HEIGHT - MARGINS.top) + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .style("text-anchor", "middle");
 
+    // Set height of visualization based on highest value
     const Y_MAX = d3.max(data, (d) => { return parseInt(d.amount); });
-    console.log(Y_MAX);
-    // Add Y axis
-    
-    let y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([FRAME_HEIGHT- MARGINS.top - MARGINS.bottom, 0]);
 
+    // Add Y axis
+    let y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([FRAME_HEIGHT- MARGINS.top - MARGINS.bottom, 0]);
     FRAME2.append("g")
         .attr("transform", "translate(" +  MARGINS.left +","+(MARGINS.top)+ ")")
         .call(d3.axisLeft(y));
 
-
-        // Bars
+    // Add Bars
     FRAME2.selectAll("mybar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("class", "bars")
-    .attr("category", function(d) {return String(d.category)})
-    .attr("amt", function(d) {return String(d.amount)})
-    .attr("x", function(d) { return x(d.category) + MARGINS.left; })
-    .attr("y", function(d) { return y(d.amount) + MARGINS.top; })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return FRAME_HEIGHT- MARGINS.top - MARGINS.bottom - y(d.amount); })
-    .attr("fill", "lavender");
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("class", "bars")
+        .attr("category", function(d) {return String(d.category)})
+        .attr("amt", function(d) {return String(d.amount)})
+        .attr("x", function(d) { return x(d.category) + MARGINS.left; })
+        .attr("y", function(d) { return y(d.amount) + MARGINS.top; })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return FRAME_HEIGHT- MARGINS.top - MARGINS.bottom - y(d.amount); })
+        .attr("fill", "lavender");
 
 
-   
+    // Create tooltip for the barchart
     let tooltip = d3.select("#barchart")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "lightgray")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "5px");
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "lightgray")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "5px");
 
 
 
@@ -213,18 +212,5 @@ d3.csv("data/bar-data.csv").then((data) => {
 
 });
 
-// Button inputs in right section
-const FRAME3 = d3.select("#selection") 
-.append("div")
-.attr("id", "selection"); 
-
-// FRAME3.append("input")
-// .attr("type", "number")
-// .attr("id", "xRange")
-// // .attr("min", "1")
-// // .attr("max", "9")
-// .attr("value", "Select X-Value 1-9");
-let inputX = d3.select(".xRange");
-console.log(inputX)
 
 
